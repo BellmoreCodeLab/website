741a9429c1953adeeae8297ba90ff395fa941163
Glenn Ferrie <glenn@gfdata.io>
2020-06-29 15:24:20 -0400

    initial check-in

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bclweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
